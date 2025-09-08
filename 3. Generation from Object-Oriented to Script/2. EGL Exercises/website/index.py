bc9c967802f03f32d1fdc5a4e1d5f839f7bf4af9
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-09-08 17:29:17 -0400

    python script

--- 3. Generation from Object-Oriented to Script/2. EGL Exercises/website/index.py
+++ 3. Generation from Object-Oriented to Script/2. EGL Exercises/website/index.py
@@ -1,81 +1,58 @@
 class Object_Oriented:
 	def __init__(self, oo_class, *args):
 		if oo_class == "School":
+			i = 0
 			self.class_name = "School"
-			self.oo_name = args[0]
-			
-			[org.eclipse.emf.ecore.impl.DynamicEObjectImpl@69bc4f0a (eClass: org.eclipse.emf.ecore.impl.EClassImpl@6ff84d82 (name: Attribute) (instanceClassName: null) (abstract: false, interface: false)), org.eclipse.emf.ecore.impl.DynamicEObjectImpl@54dc2235 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@34efeb6e (name: Reference) (instanceClassName: null) (abstract: false, interface: false)), org.eclipse.emf.ecore.impl.DynamicEObjectImpl@7965f171 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@6ff84d82 (name: Attribute) (instanceClassName: null) (abstract: false, interface: false))]
-			
-			
-			
-			
+			self.oo_name = args[i]
+			self.attribute = {}
+			self.reference = {}
+			i += 1
+			self.attribute["address"] = args[i]
+			i += 1
+			self.reference["students"] = args[i]
+			i += 1
+			self.attribute["postCodesAccepted"] = args[i]
 		if oo_class == "Student":
+			i = 0
 			self.class_name = "Student"
-			self.oo_name = args[0]
-			
-			[org.eclipse.emf.ecore.impl.DynamicEObjectImpl@40bac4b6 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@6ff84d82 (name: Attribute) (instanceClassName: null) (abstract: false, interface: false))]
-			
-			
-			
-			
+			self.oo_name = args[i]
+			self.attribute = {}
+			self.reference = {}
+			i += 1
+			self.attribute["grade"] = args[i]
 		if oo_class == "Person":
+			i = 0
 			self.class_name = "Person"
-			self.oo_name = args[0]
-			
-			[org.eclipse.emf.ecore.impl.DynamicEObjectImpl@4bf517b4 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@6ff84d82 (name: Attribute) (instanceClassName: null) (abstract: false, interface: false))]
-			
-			
-			
-			
+			self.oo_name = args[i]
+			self.attribute = {}
+			self.reference = {}
+			i += 1
+			self.attribute["details"] = args[i]
 		if oo_class == "Employee":
+			i = 0
 			self.class_name = "Employee"
-			self.oo_name = args[0]
-			
-			[org.eclipse.emf.ecore.impl.DynamicEObjectImpl@7e0d4975 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@6ff84d82 (name: Attribute) (instanceClassName: null) (abstract: false, interface: false))]
-			
-			
-			
-			
+			self.oo_name = args[i]
+			self.attribute = {}
+			self.reference = {}
+			i += 1
+			self.attribute["salary"] = args[i]
 		if oo_class == "Teacher":
+			i = 0
 			self.class_name = "Teacher"
-			self.oo_name = args[0]
-			
-			[org.eclipse.emf.ecore.impl.DynamicEObjectImpl@2835c1bc (eClass: org.eclipse.emf.ecore.impl.EClassImpl@34efeb6e (name: Reference) (instanceClassName: null) (abstract: false, interface: false))]
-			
-			
-			
-			
+			self.oo_name = args[i]
+			self.attribute = {}
+			self.reference = {}
+			i += 1
+			self.reference["teaches"] = args[i]
 		if oo_class == "Class":
+			i = 0
 			self.class_name = "Class"
-			self.oo_name = args[0]
-			
-			[org.eclipse.emf.ecore.impl.DynamicEObjectImpl@3e3c2f78 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@34efeb6e (name: Reference) (instanceClassName: null) (abstract: false, interface: false))]
-			
-			
-			
-			
-        #self.attribute = {}
-        #self.reference = {}
-        
-        
-
-        #self.attribute = self.get_attributes(**kwargs)
-        #self.reference = self.get_reference(**kwargs)
-	'''
-    def get_features(self, *args):
-    	
-
-        if kwargs:
-            for key, value in kwargs.items():
-                self.attribute[key] = value
-        return self.attribute
-
-    def get_reference(self, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                self.reference[key] = value
-        return self.reference
-	'''
+			self.oo_name = args[i]
+			self.attribute = {}
+			self.reference = {}
+			i += 1
+			self.reference["students"] = args[i]
+		
 	
 def main():
     
@@ -84,6 +61,8 @@ def main():
 
     # Access attributes
     print(f"My school's name is {my_school.oo_name}.")
+    print(f"My school's address is {my_school.attribute['address']}.")
+    print(f"{my_school.reference['students']} is one of the student from my school.")
 
 if __name__ == "__main__":
     main()
