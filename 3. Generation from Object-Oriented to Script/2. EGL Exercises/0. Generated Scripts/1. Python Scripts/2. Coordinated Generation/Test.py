499fe597b3be0362d6b536b73359eaf4c3d4fdc9
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-09-09 17:34:49 -0400

    Update Test.py

--- 3. Generation from Object-Oriented to Script/2. EGL Exercises/0. Generated Scripts/1. Python Scripts/2. Coordinated Generation/Test.py
+++ 3. Generation from Object-Oriented to Script/2. EGL Exercises/0. Generated Scripts/1. Python Scripts/2. Coordinated Generation/Test.py
@@ -10,17 +10,17 @@ def main():
     # Create instances of each class
 
     person = Person("John Doe", "1990-01-01")
-    print(f"Person:\n\tID = {person.person_id},\n\tDetails = {person.details}\n")
+    print(f"Person:\n\tPerson ID = {person.person_id},\n\tDetails = {person.details}\n")
 
     student = Student("John Doe", "1990-01-01", "20050515", "L3")
     print(
-        f"Student:\n\tID = {student.person_id},\n\tDetails = {student.details}\n\t"
+        f"Student:\n\tPerson ID = {student.person_id},\n\tDetails = {student.details}\n\t"
         f"Student ID = {student.student_id},\n\tGrade = {student.grade}\n"
     )
 
     employee = Employee("Alice Johnson", "1985-03-22", "20250515", 50000)
     print(
-        f"Employee:\n\tID = {employee.person_id},\n\tDetails = {employee.details}\n\t"
+        f"Employee:\n\tPerson ID = {employee.person_id},\n\tDetails = {employee.details}\n\t"
         f"Employee ID = {employee.employee_id},\n\tSalary = {employee.salary}\n"
     )
 
@@ -28,14 +28,14 @@ def main():
         "Alice Johnson", "1985-03-22", "20250515", 50000, "0515", ["201", "203", "204"]
     )
     print(
-        f"Teacher:\n\tID = {teacher.person_id},\n\tDetails = {teacher.details}\n\t"
+        f"Teacher:\n\tPerson ID = {teacher.person_id},\n\tDetails = {teacher.details}\n\t"
         f"Employee ID = {teacher.employee_id},\n\tSalary = {teacher.salary}\n\t"
         f"Teacher ID = {teacher.teacher_id},\n\tSubjects = {teacher.teaches_id}\n"
     )
 
     class_instance = Class("201", ["20052001", "20052002", "20052003"])
     print(
-        f"Class:\n\tID = {class_instance.class_id},\n\tStudents = {class_instance.students_id}\n"
+        f"Class:\n\tClass ID = {class_instance.class_id},\n\tStudents = {class_instance.students_id}\n"
     )
 
     school = School(
@@ -45,7 +45,7 @@ def main():
         "012",
     )
     print(
-        f"School:\n\tID = {school.school_id},\n\tAddress = {school.address}\n\t"
+        f"School:\n\tSchool ID = {school.school_id},\n\tAddress = {school.address}\n\t"
         f"Students = {school.students_id},\n\tPost Codes Accepted = {school.postCodesAccepted}\n"
     )
 
